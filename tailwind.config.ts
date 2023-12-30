0eeb32f512bfdb2d35251f1f6e1846f306f073f1
Liago <liago@dev.me>
2023-12-30 18:29:02 +0100

    (feat) installed "aspect/ratio" plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss";
 const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
-  
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,7 +10,7 @@ const config: Config = {
   ],
   theme: {
     letterSpacing: {
-      widest: '.3em',
+      widest: ".3em",
     },
     extend: {
       backgroundImage: {
@@ -19,12 +18,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors: { 
-        'fake-black': '#6a5921'
+      colors: {
+        "fake-black": "#6a5921",
       },
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [nextui(), require("@tailwindcss/aspect-ratio")],
 };
 export default config;
