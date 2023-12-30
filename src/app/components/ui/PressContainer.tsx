86ad586e9cec9c1c21246e33424438aa4318dee0
Liago <liago@dev.me>
2023-12-30 18:29:30 +0100

    (fix) fixed Press container

--- src/app/components/ui/PressContainer.tsx
+++ src/app/components/ui/PressContainer.tsx
@@ -1,9 +1,22 @@
+import { cinzel } from "@/assets/fonts";
+import { Link } from "@nextui-org/react";
 import Image from "next/image";
 
 const PressContainer = () => {
   const pressCover = () => {
     return (
       <div className="relative block w-full">
+        <div
+          className={`absolute top-1/2 left-10 xl:bottom-56 xl:left-20 ${cinzel.className}`}
+        >
+          <Link
+            className="text-3xl xl:text-5xl text-white"
+            showAnchorIcon
+            href="#"
+          >
+            Rassegna Stampa
+          </Link>
+        </div>
         <Image
           alt="press cover"
           src={process.env.PRESS_COVER_URL as string}
